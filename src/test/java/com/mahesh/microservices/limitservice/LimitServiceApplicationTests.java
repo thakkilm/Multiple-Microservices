b309f79c0097574c9781dcefa0806fe50ebcf426
MAHESH THAKKILAPATI <maheshthakkilapati@MAHESHs-MBP.attlocal.net>
2024-03-15 12:33:01 -0500

    Initialize new project linit-service

---
+++ src/test/java/com/mahesh/microservices/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.microservices.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
